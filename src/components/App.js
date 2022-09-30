63290aceb1eb64741d9442de0ef6f210fef36c0e
Victor Barros <coffiekingb@gmail.com>
2022-09-30 00:35:27 -0300

    :sparkles: Feat:  Create User Context

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
-import LoginPage from "./LoginPage/LoginPage";
+import LoginPage from "./AuthComponent/LoginPage";
 import GlobalStyled from "./GlobalStyle/globalStyled";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SingupPage from "./SingUpPage/SignupPage";
+import SingupPage from "./AuthComponent/SignupPage";
+import { userContext } from "../context/userContext";
+import { useState } from "react";
 function App() {
+  const [userData, setUserData] = useState({});
   return (
-    <BrowserRouter>
-      <GlobalStyled />
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/sign-up" element={<SingupPage />} />
-      </Routes>
-    </BrowserRouter>
+    <userContext.Provider value={{ userData, setUserData }}>
+      <BrowserRouter>
+        <GlobalStyled />
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/sign-up" element={<SingupPage />} />
+        </Routes>
+      </BrowserRouter>
+    </userContext.Provider>
   );
 }
 
