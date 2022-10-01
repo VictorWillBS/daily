813093bcf558c7d1aeb371cbda53fa0086377a51
Victor Barros <coffiekingb@gmail.com>
2022-10-01 01:53:03 -0300

    :wrench: Chore: Move css to NavBarrStyle.js

--- src/components/NavBarr/NavBar.js
+++ src/components/NavBarr/NavBar.js
@@ -1,4 +1,9 @@
-import styled from "styled-components";
+import {
+  UserArticle,
+  PageNameArticle,
+  TitleArticle,
+  NavBarrContainer,
+} from "./NavBarStyle";
 export default function NavBarr() {
   const pageName = "Responder";
   return (
@@ -16,68 +21,3 @@ export default function NavBarr() {
     </NavBarrContainer>
   );
 }
-
-const NavBarrContainer = styled.section`
-  min-width: 300px;
-  width: 100vw;
-  height: 107px;
-  padding: 0 1em;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  border-radius: 0 0 10px 10px;
-  background-color: #003b76;
-  position: absolute;
-  left: 0;
-  top: 0;
-`;
-
-const TitleArticle = styled.article`
-  width: max-content;
-  height: fit-content;
-  padding-top: 7px;
-  h1 {
-    color: #ffffff;
-    font-size: 55px;
-    font-weight: 600;
-    text-shadow: 3px 5px 10px #000;
-    font-family: "Kufam", sans-serif;
-    text-align: start;
-  }
-  .sm {
-    display: none;
-  }
-  span {
-    color: #ffcd8c;
-  }
-  @media (min-width: 0) and (max-width: 600px) {
-    .lg {
-      display: none;
-    }
-    .sm {
-      display: flex;
-    }
-  }
-`;
-
-const PageNameArticle = styled.article`
-  h1 {
-    color: white;
-    font-family: "Roboto", sans-serif;
-    font-size: 25px;
-    font-weight: 700;
-  }
-`;
-
-const UserArticle = styled.article`
-  width: 60px;
-  height: 60px;
-  border-radius: 100%;
-  background-color: white;
-  img {
-    width: 60px;
-    height: 60px;
-    object-fit: cover;
-    border-radius: 100%;
-  }
-`;
--- src/components/NavBarr/NavBarStyle.js
+++ src/components/NavBarr/NavBarStyle.js
@@ -1,9 +1,66 @@
 import styled from "styled-components";
-export const TotalContainer = styled.section`
+
+export const NavBarrContainer = styled.section`
+  min-width: 300px;
   width: 100vw;
-  height: 100vh;
+  height: 107px;
+  padding: 0 1em;
   display: flex;
-  justify-content: space-between;
   align-items: center;
+  justify-content: space-between;
+  border-radius: 0 0 10px 10px;
   background-color: #003b76;
+  position: absolute;
+  left: 0;
+  top: 0;
+`;
+
+export const TitleArticle = styled.article`
+  width: max-content;
+  height: fit-content;
+  padding-top: 7px;
+  h1 {
+    color: #ffffff;
+    font-size: 55px;
+    font-weight: 600;
+    text-shadow: 3px 5px 10px #000;
+    font-family: "Kufam", sans-serif;
+    text-align: start;
+  }
+  .sm {
+    display: none;
+  }
+  span {
+    color: #ffcd8c;
+  }
+  @media (min-width: 0) and (max-width: 600px) {
+    .lg {
+      display: none;
+    }
+    .sm {
+      display: flex;
+    }
+  }
+`;
+
+export const PageNameArticle = styled.article`
+  h1 {
+    color: white;
+    font-family: "Roboto", sans-serif;
+    font-size: 25px;
+    font-weight: 700;
+  }
+`;
+
+export const UserArticle = styled.article`
+  width: 60px;
+  height: 60px;
+  border-radius: 100%;
+  background-color: white;
+  img {
+    width: 60px;
+    height: 60px;
+    object-fit: cover;
+    border-radius: 100%;
+  }
 `;
