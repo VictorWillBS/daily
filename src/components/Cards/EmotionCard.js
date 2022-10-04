9772c4afb273f86c791f6bade2e6f7b4270e1904
Victor Barros <coffiekingb@gmail.com>
2022-10-04 02:35:44 -0300

    :construction:Feat: Emotion Card post

--- src/components/Cards/EmotionCard.js
+++ src/components/Cards/EmotionCard.js
@@ -1,12 +1,23 @@
 import styled from "styled-components";
 import { IconContext } from "react-icons";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ImSmile, ImSad, ImNeutral } from "react-icons/im";
+import generateDate from "../../functions/generateDate";
+import { userContext } from "../../context/userContext";
+import axios from "axios";
 
 export default function EmotionCard() {
+  const day = generateDate(new Date());
   const [felling, setFelling] = useState("");
   const [color, setColor] = useState("");
   const [emoji, setEmoji] = useState("");
+  const [answered, setAnswered] = useState(false);
+  const { userData } = useContext(userContext);
+  const config = {
+    headers: {
+      Authorization: `Bearer ${userData.token}`,
+    },
+  };
   useEffect(() => {
     switch (felling) {
       case "Triste":
@@ -23,19 +34,50 @@ export default function EmotionCard() {
         setEmoji(<ImSmile />);
         break;
       default:
+        setEmoji("");
+
         return;
     }
   }, [felling]);
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_URL_API}/felling/today`, config)
+      .then((res) => {
+        if (res.data.length) {
+          setAnswered(true);
+        }
+      });
+  }, []);
+
   function inputControl(e) {
     console.log();
     setFelling(e.target.value);
   }
+
+  async function submit(e, day, felling) {
+    e.preventDefault();
+    if (answered) {
+      return;
+    }
+    const body = {
+      felling,
+      day,
+    };
+
+    await axios
+      .post(`${process.env.REACT_APP_URL_API}/felling`, body, config)
+      .then(() => {
+        setAnswered(true);
+      });
+  }
+
   return (
     <Container>
       <EmotionFace color={color}>
         <figure
           onClick={(e) => {
-            e.preventDefault();
+            submit(e, day, felling);
           }}
         >
           <IconContext.Provider
@@ -58,9 +100,11 @@ export default function EmotionCard() {
           type="option"
           placeholder="Como você se sente?"
           list="sentimentos"
+          disabled={answered}
           value={felling}
           onChange={inputControl}
         >
+          <option value="">Como foi seu Dia?</option>
           <option value="Alegre">Aconteceram coisas boas hoje.</option>
           <option value="Neutro">Foi um dia Neutro.</option>
           <option value="Triste">Aconteceram coisas ruins hoje.</option>
