f4c707a69c13772b8490a89129dac360f36c37a3
Victor Barros <coffiekingb@gmail.com>
2022-10-07 15:45:48 -0300

    :package: feat: Message alert

--- src/components/AuthComponent/LoginPage.js
+++ src/components/AuthComponent/LoginPage.js
@@ -7,18 +7,22 @@ import {
   BGFigure,
   BGImage,
 } from "./authStyle";
+import MessageAlert from "../MessageAlert/MessageAlert";
+import { useState } from "react";
 export default function LoginPage() {
+  const [alert, setAlert] = useState();
   return (
     <TotalContainer>
       <BGFigure>
         <BGImage src={loginBg} />
       </BGFigure>
       <UserInputSide>
+        <MessageAlert alert={alert} />
         <h1>
           DAI<span>LY</span>
         </h1>
 
-        <Form isSignUp={false} />
+        <Form isSignUp={false} setAlert={setAlert} />
         <StyledLink to="/sign-up">Não tem conta ainda? Cadastre-se!</StyledLink>
       </UserInputSide>
     </TotalContainer>
