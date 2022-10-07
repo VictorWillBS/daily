a64a1f33c048adaaede14ea366a97d301bc3a0d1
Victor Barros <coffiekingb@gmail.com>
2022-10-06 23:58:21 -0300

    :sparkles:Feat: Add  calendar redirect page

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,13 +1,32 @@
-import { CategoryStyled, MenuStyled } from "./MenuStyle";
+import { CategoryStyled, MenuStyled, TotalContainer } from "./MenuStyle";
 import {
   IoAddCircleOutline,
   IoCalendarOutline,
   IoMailOutline,
+  IoChevronBackOutline,
+  IoLogOutOutline,
 } from "react-icons/io5";
+import generateDate from "../../functions/generateDate";
+
 import { IconContext } from "react-icons";
 import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
-function Category({ categories }) {
+function redirect(categoryData, navigate) {
+  if (categoryData.name === "logout") {
+    localStorage.clear();
+    navigate(categoryData.link);
+    return;
+  }
+  if (categoryData.name === "Calendário") {
+    const date = generateDate(new Date());
+    navigate(`${categoryData.link}/${date}`);
+    return;
+  }
+  navigate(categoryData.link);
+}
+
+function Category({ categories, menuActive }) {
   const navigate = useNavigate();
   const hashCategories = {
     responder: {
@@ -18,21 +37,26 @@ function Category({ categories }) {
     calendario: {
       name: "Calendário",
       icon: <IoCalendarOutline />,
-      link: "/testeCal",
+      link: "/day",
     },
     mensagem: {
       name: "Mensagem",
       icon: <IoMailOutline />,
       link: "/testeMen",
     },
+    logout: {
+      name: "logout",
+      icon: <IoLogOutOutline />,
+      link: "/sign-in",
+    },
   };
   return categories.map((category) => {
     return (
-      <article>
+      <article className={`category-${menuActive}`}>
         <div>
           <figure
             onClick={() => {
-              navigate(hashCategories[category].link);
+              redirect(hashCategories[category], navigate);
             }}
           >
             {hashCategories[category].icon}
@@ -45,24 +69,45 @@ function Category({ categories }) {
     );
   });
 }
+function togleActive(menuActive, setMenuActive) {
+  if (menuActive === "not-active") {
+    setMenuActive("active");
+  } else {
+    setMenuActive("not-active");
+  }
+}
 
 export default function Menu() {
+  const [menuActive, setMenuActive] = useState("not-active");
   return (
-    <MenuStyled>
-      <IconContext.Provider
-        value={{
-          style: {
-            width: "28px",
-            height: "28px",
-            color: "white",
-            fontWeight: "bolder",
-          },
-        }}
-      >
-        <CategoryStyled>
-          <Category categories={["responder", "calendario", "mensagem"]} />
-        </CategoryStyled>
-      </IconContext.Provider>
-    </MenuStyled>
+    <TotalContainer>
+      <MenuStyled className={`${menuActive}`}>
+        <IconContext.Provider
+          value={{
+            style: {
+              width: "28px",
+              height: "28px",
+              color: "white",
+              fontWeight: "bolder",
+            },
+          }}
+        >
+          <figure
+            className="mobile-arrow"
+            onClick={() => {
+              togleActive(menuActive, setMenuActive);
+            }}
+          >
+            <IoChevronBackOutline />
+          </figure>
+          <CategoryStyled>
+            <Category
+              menuActive={menuActive}
+              categories={["responder", "calendario", "mensagem", "logout"]}
+            />
+          </CategoryStyled>{" "}
+        </IconContext.Provider>
+      </MenuStyled>
+    </TotalContainer>
   );
 }
