34cedc0d549710669672d17139effd31a221f6bb
Victor Barros <coffiekingb@gmail.com>
2022-10-07 15:45:20 -0300

    :package: feat: Add Help Box and Message alert

--- src/components/AuthComponent/SignupPage.js
+++ src/components/AuthComponent/SignupPage.js
@@ -6,21 +6,43 @@ import {
   StyledLink,
   BGFigure,
   BGImage,
+  HelpSection,
+  HelpText,
 } from "./authStyle";
+import { useState } from "react";
+import MessageAlert from "../MessageAlert/MessageAlert";
 
 export default function SingupPage() {
+  const [inputdata, setInputdata] = useState({ email: false, password: false });
+  const [alert, setAlert] = useState();
+
   return (
     <TotalContainer>
       <BGFigure>
         <BGImage src={loginBg} />
       </BGFigure>
       <UserInputSide>
+        <MessageAlert alert={alert} />
         <h1>
           DAI<span>LY</span>
         </h1>
 
-        <Form isSignUp={true} />
-        <StyledLink to="/">Não tem conta ainda? Cadastre-se!</StyledLink>
+        <Form
+          isSignUp={true}
+          setInputdata={setInputdata}
+          inputdata={inputdata}
+          setAlert={setAlert}
+        />
+        <StyledLink to="/sign-in">Não tem conta ainda? Cadastre-se!</StyledLink>
+        <HelpSection>
+          <HelpText>Photo: Não é obrigatória.</HelpText>
+          <HelpText correct={inputdata.email}>
+            Email: Deve ser um email valido.
+          </HelpText>
+          <HelpText correct={inputdata.password}>
+            Senha: Deve ter entre 10 - 30 caracteres.
+          </HelpText>
+        </HelpSection>
       </UserInputSide>
     </TotalContainer>
   );
