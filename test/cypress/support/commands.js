59d08613364461b4546b1b4f5159ad2834c8bb1c
Victor Barros <coffiekingb@gmail.com>
2022-10-10 00:21:01 -0300

    Add Generate Token command

--- test/cypress/support/commands.js
+++ test/cypress/support/commands.js
@@ -10,7 +10,28 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
+import { faker } from "@faker-js/faker";
+Cypress.Commands.add("resetDataBase", () => {
+  cy.request("POST", "localhost:5000/clearbank");
+});
+Cypress.Commands.add("generateToken", () => {
+  const user = {
+    email: faker.internet.email(),
+    name: "test",
+    password: "1234567890",
+    photo:
+      "https://64.media.tumblr.com/6ed4d905e2daf140621cf49fb4e992a7/45c8f5f1c04730ea-f6/s1280x1920/f32aeed9ad2cab1e11e0245d19729abbdab3bb70.png",
+  };
+  const { email, password } = user;
+  cy.request("POST", "localhost:5000/signup", user);
+  let token;
+  cy.request("POST", "localhost:5000/signin", { email, password }).then(
+    (response) => {
+      localStorage.setItem("token", response.body.token);
+    }
+  );
+});
+
 //
 //
 // -- This is a child command --
